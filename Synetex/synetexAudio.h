eb937309cfba228bbe0e10d52fb8d24b54cffc27
kohlkuntze <kohlkuntze@gmail.com>
2019-01-13 17:44:01 -0500

    Audio Headerfile

---
+++ Synetex/synetexAudio.h
@@ -0,0 +1,11 @@
+//
+//  synetexAudio.c
+//  Synetex
+//
+//  Created by Kohl Kuntze on 1/13/19.
+//  Copyright © 2019 Daniel Freyschlag. All rights reserved.
+//
+
+#include <stdio.h>
+
+void updateVolume(float frequency, float volume);
