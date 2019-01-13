afb15e43b47fd9cae6ab2a9837487a2d0cdcfb13
Danny <dfreyschlag1@gmail.com>
2019-01-13 17:19:57 -0500

    xCode Stuff

---
+++ Synetex/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Synetex
+//
+//  Created by Daniel Freyschlag on 1/13/19.
+//  Copyright © 2019 Daniel Freyschlag. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
